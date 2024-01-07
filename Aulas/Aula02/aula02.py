b07f0534158caeacd408b660f75abaa39e87bd1a
AndreyMoraesFriedel <AndreyMoraesFriedel@gmail.com>
2024-01-07 14:25:18 -0300

    Aula02

---
+++ Aulas/Aula02/aula02.py
@@ -0,0 +1,17 @@
+#Python é uma linguagem com tipagem dinamica:
+
+""" 
+num=1
+num="HelloWorld"
+num=7.5
+"""
+
+#Concatenação de String
+nome = "Andrey"
+curso = "Python"
+
+print(f"Bem vindo {nome} ao curso de {curso}")
+
+if 10 > 2:
+ print("Maior")
+print("FIM")
