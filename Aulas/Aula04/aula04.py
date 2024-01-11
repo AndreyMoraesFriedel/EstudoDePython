0fae56ef3592d230ad9127f39cccfe6a1d67481c
AndreyMoraesFriedel <AndreyMoraesFriedel@gmail.com>
2024-01-11 19:20:21 -0300

    Aula04 - Tipo de Dados

---
+++ Aulas/Aula04/aula04.py
@@ -0,0 +1,17 @@
+x=1 #int
+x="Andrey" #string
+x=15.7 #float
+x=True #bool
+x=["Carro", "Moto", "Aviao", 1, 58.3, True] #List / Array
+x=("Carro", "Moto", "Aviao", 1, 58.3, True) #Tupla
+x=range(0,100,1) #Criacao de list
+x={ #Dictionary
+    "nome":"andrey",
+    "curso":"python"
+}
+x={5, 7, 4, 7, 6, 93, 5}#set
+x=frozenset({5, 7, 4, 7, 6, 93, 5})
+print("Valor: " + str(x))
+
+
+print("Tipo: " + str(type(x)))
