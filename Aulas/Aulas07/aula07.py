9b05675517e305356bb4e998f9ac80b171d5a142
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2024-01-18 18:56:32 -0300

    Aula07 - String Part2

---
+++ Aulas/Aulas07/aula07.py
@@ -0,0 +1,9 @@
+city = "Blumenau"
+day = 12
+mouth = "April"
+year = 2006
+
+#res = palavra.upper() not in texto.upper()
+
+#print(f"Cidade {city}, dia {day}, mês {mouth}, ano {year}")
+print("Cidade {}, dia {}, mês {}, ano {}".format(city,day,mouth,year))
