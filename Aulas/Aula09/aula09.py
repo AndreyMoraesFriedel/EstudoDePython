24b1582437f8eb468e9911b6e0c42dc8f59d031f
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2024-01-23 19:56:17 -0300

    Aula09 - Coleção List

---
+++ Aulas/Aula09/aula09.py
@@ -0,0 +1,20 @@
+listaDeCarros=["parati", "gol", "voyage","santana", "saveiro", "virtus", "jetta"]
+
+listaDeMotos=["cavazaque", "xj6", "hornetao"]  
+
+listaDeVeiculos=listaDeCarros + listaDeMotos #Fundir duas listas
+
+#backupListaDeCarrosDaVolks=list(listaDeCarrosDaVolks); #Copiar Uma Lista
+#listaDeCarrosDaVolks.remove("gol"); #Remover Elemento da List
+#del listaDeCarrosDaVolks[2]         #Outra forma de Remover Elemnto da List
+#listaDeCarrosDaVolks.pop();         #Remover ultimo Elemento da List
+#listaDeCarrosDaVolks.clear()        #Remove todos elementos da Lista
+#listaDeCarrosDaVolks.append("tiguan") #Adicionar Elemento da List
+
+
+
+#print(listaDeCarrosDaVolks[-4]);   #Utilizando valores negativos, pode se ler a lista da direita para esquerda
+
+#print(str(len(backupListaDeCarrosDaVolks)) + " carros na lista") #Como mostrar tamanho da lista
+
+print(listaDeVeiculos)
